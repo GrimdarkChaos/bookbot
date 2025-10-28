c5f2d258e719c0f638f5a75b207205df3a0884e0
GrimdarkChaos <randy@lennonverse.com>
2025-10-28 13:19:14 -0400

    Added a function to return the word count and changed the output to print the word count instead of the book contents.

--- main.py
+++ main.py
@@ -4,9 +4,16 @@ def get_book_text(path_to_file):
         file_contents = f.read()
     return file_contents
 
+#Take the book contents and return the word count
+def num_of_words(file_contents):
+    word_count = len(file_contents.split())
+    return word_count
+
 #Define main
-def main(file_path):
-    print(get_book_text(file_path))
+def main(path_to_file):
+    file_contents = get_book_text(path_to_file)
+    word_count = num_of_words(file_contents)
+    print(f"Found {word_count} total words")
 
 #Calling main
 main("./books/frankenstein.txt")
