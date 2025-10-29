ad824767df39bb517cdbef51599e41eee9091eb9
GrimdarkChaos <randy@lennonverse.com>
2025-10-28 20:56:46 -0400

    completed project with arguments

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 #Import functions
 from stats import num_of_words, count_characters, sorted_list
+import sys
 
 #Define file path
-path_to_file = "books/frankenstein.txt"
+#path_to_file = "books/frankenstein.txt"
 
 #Take the file path and open the file returning the contents as a string
 def get_book_text(path_to_file):
@@ -12,19 +13,25 @@ def get_book_text(path_to_file):
 
 #Define main
 def main(path_to_file):
-    file_contents = get_book_text(path_to_file)
-    word_count = num_of_words(file_contents)
-    character_count = count_characters(file_contents)
-    sorted_character_count = sorted_list(character_count)
-    print(f"============ BOOKBOT ============")
-    print(f"Analyzing book found at {path_to_file}")
-    print("----------- Word Count ----------")
-    print(f"Found {word_count} total words")
-    print("--------- Character Count -------")
-    for character in sorted_character_count:
-        if character["char"].isalpha():
-            print(f"{character['char']}: {character['num']}")
-    print("============= END ===============")
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    else:
+        path_to_file = sys.argv[1]
+        file_contents = get_book_text(path_to_file)
+        word_count = num_of_words(file_contents)
+        character_count = count_characters(file_contents)
+        sorted_character_count = sorted_list(character_count)
+        print(f"============ BOOKBOT ============")
+        print(f"Analyzing book found at {path_to_file}")
+        print("----------- Word Count ----------")
+        print(f"Found {word_count} total words")
+        print("--------- Character Count -------")
+        for character in sorted_character_count:
+            if character["char"].isalpha():
+                print(f"{character['char']}: {character['num']}")
+        print("============= END ===============")
 
 #Calling main
-main(path_to_file)
+if __name__ == "__main__":
+    main(sys.argv[1:])
